a7b33dbcf3a74210a54514cc841d7c758c8bfd6c
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2018-10-26 10:11:46 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,6 @@ exports.run = (bot, message, args, prefix) => {
 
     let embed = new Discord.RichEmbed()
     .setColor("BLUE")
-    .addField('```Latency: ```', new Date().getTime() - message.createdTimestamp + " ms ")
+    .addField('```Hello, Professor! My heart rate is ```', new Date().getTime() - message.createdTimestamp + " ms ")
     message.channel.send(embed)
 }
