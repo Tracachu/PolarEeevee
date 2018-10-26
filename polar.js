2341cdb9157fc0c5ad3d7491d22b2a7bfc8a325d
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2018-10-26 17:03:59 -0400

    Update polar.js

--- polar.js
+++ polar.js
@@ -44,6 +44,20 @@ console.log(`OwnerID: ${ownerID}`)
 console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 })
 
+bot.on("message"), async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split (" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hello`){
+        return message.channel.send("Hello!")
+    }
+}
+
 bot.on("message", message => {
 
     let args = message.content.slice(prefix.length).trim().split(" ");
