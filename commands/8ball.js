37ec2b76ab44cd2754ea4b40037d0d8ff601f680
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2018-10-26 10:07:01 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -6,7 +6,7 @@ exports.run = (bot, message, args, prefix) => {
     if(message.author.bot) return;
 
     if(!args[0]) return message.reply("Please give me a question!");
-    let replies = ["Of Course", "My database says No", "There could be a chance", "Please ask again later", "I don\'t know", "Possibly", "The Stars Say Yes", "The Stars Say No", "Wait And See"];
+    let replies = ["Of course", "My database says no", "Seven.", "There could be a chance", "Please ask again later", "I don't know", "Possibly", "The stars say yes", "The stars say no", "Ask again later", "Wait and see"];
     
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(0).join(" ").replace((/`/g,'\`')) + (" ");
