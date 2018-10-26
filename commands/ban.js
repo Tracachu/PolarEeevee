ba7e65b04743fd86708d3acc95681c8f0989a187
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2018-10-26 10:08:27 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,10 +6,10 @@ exports.run = (bot, message, args, prefix) => {
     if(message.author.bot) return;
 
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("Can't find user!");
+    if(!bUser) return message.channel.send("Oh, no! I can't seem to find that user! :(");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry i can't!");
-    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("That person can't be kicked!");
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("I'm not strong enough to do this! :sob:");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("OOF! This user has too many badges for you to train!");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("~Ban~")
@@ -20,8 +20,8 @@ exports.run = (bot, message, args, prefix) => {
     .addField("Time", message.createdAt)
     .addField("Reason", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "incidents");
-    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+    let incidentchannel = message.guild.channels.find(`name`, "report-logs");
+    if(!incidentchannel) return message.channel.send("I'm sorry, I can't find the channel I need. Please make a report-logs channel.");
 
     message.guild.member(bUser).ban(bReason);
     incidentchannel.send(banEmbed);
