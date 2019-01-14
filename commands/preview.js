9bffec56be89366e0beb6f7ef186f0484eed3f13
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2019-01-14 14:56:10 -0500

    Update preview.js

--- commands/preview.js
+++ commands/preview.js
@@ -1,28 +1,32 @@
 
 const Discord = require("discord.js");
-oakdexPokedex = require("oakdex-pokedex");
+let pokemonData = require("previewData.json");
 
 exports.run = (bot, message, args, prefix) => {
 
     if(message.author.bot) return;
 
-    function ucFirst(string) 
-    {
-        return string.charAt(0).toUpperCase() + string.slice(1);
+    function capitalLetters(wordOne, wordTwo) {
+
+        return wordOne.charAt(0).toUpperCase() + args[0].slice(1) && wordTwo.charAt(0).toUpperCase() + args[1].slice(1);
     }
 
-    
 
-   
+        let p = pokemonData[capitalLetters(args[0], args[1])][0];
 
-    oakdexPokedex.findPokemon(ucFirst(args.join(" ")), function(p) {
 
         let embed = new Discord.RichEmbed()
         .setColor("BLUE")
         .setTitle(`Pokedex results for ` + `\`\`\`${ucFirst(args.join(" "))}\`\`\``)
+        .addField("National Dex Number:", p.National_dex, true)
+        .addField("Primary Type:", p.primary_type, true)
+        .addField("Secondary Type:", p.secondary_type, true)
+        .addField("Rarity:", p.rarity, true)
+        .addField("Classification:", p.Classification, true)
+        .addField("Evolves by:", p.evolves_by, true)
+        .addField("Evolves into:", p.evolves_into, true)
         .setThumbnail(`https://play.pokemonshowdown.com/sprites/xyani/${args.join(" ").toLowerCase()}.gif`)
-        .setImage(`https://play.pokemonshowdown.com/sprites/xyani-shiny/${args.join(" ").toLowerCase()}.gif`)
+        .setImage(`https://play.pokemonshowdown.com/sprites/xyani-shiny/${args.join(" ").toLowerCase()}.gif`);
         message.channel.send(embed)
 
-      });
 }
