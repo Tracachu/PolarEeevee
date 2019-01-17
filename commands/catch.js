fd8381bf3082a5ac3e2b132dec9164547367653a
TracaPoo <42890509+Tracachu@users.noreply.github.com>
2019-01-17 20:40:51 +0000

    Create catch.js

---
+++ commands/catch.js
@@ -0,0 +1,52 @@
+
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args, prefix) => {
+
+    if(message.author.bot) return;
+ 
+    
+    if(!message.mentions.users.first()) {
+
+        let embed1 = new Discord.RichEmbed()
+        .setColor("BLACK")
+        .setTitle("You have to tag someone in order to catch them!")
+        .setImage("https://i.imgur.com/eBUjz6w.gif")
+        message.channel.send(embed1)
+
+        return;
+        
+    }
+    
+
+    let replies = ["emebd2", "embed3", "embed4"];
+    
+    let user = message.mentions.users.first();
+    let member = bot.guild.member(user.id);
+
+    let embed2 = new Discord.RichEmbed()
+    .setColor("RED")
+    .setTitle(`Success! The wild ${member.user.tag} was caught!`) 
+    .setImage("https://i.imgur.com/Y2auGcZ.gif");
+
+    let embed3 = new Discord.RichEmbed()
+    .setColor("RED")
+    .setTitle(`Looks like you tried a little too hard there...`)
+    .setImage("https://i.imgur.com/XD8e7al.gif");
+    
+    let embed4 = new Discord.RichEmbed()
+    .setColor("RED")
+    .setTitle(`Ono, it looks like ${member.user.tag} doesn't like their new home...`) 
+    .setImage("https://i.imgur.com/Q04RAEl.gif");
+
+
+    let result = Math.floor((Math.random() * replies.length));
+
+    message.channel.send(replies[result]); 
+
+
+    
+    
+}
+
+//np
