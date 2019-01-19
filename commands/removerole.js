63ffa9b52efb3538ede4396da13289714b2f62e3
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2019-01-19 16:53:10 -0500

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry, but you're not allowed to tamper with roles.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!rMember) return message.reply("Couldn't find that user, yo.");
+  if(!rMember) return message.reply("Couldn't find that user to fiddle with. Try a flute instead.");
   let role = args.join(" ").slice(22);
-  if(!role) return message.reply("Specify a role!");
+  if(!role) return message.reply("Can't do anything if you don't tell me what role I'm snatching.");
   let gRole = message.guild.roles.find(`name`, role);
-  if(!gRole) return message.reply("Couldn't find that role.");
+  if(!gRole) return message.reply("Yeah uh... that role doesn't exist.");
 
-  if(!rMember.roles.has(gRole.id)) return message.reply("They don't have that role.");
+  if(!rMember.roles.has(gRole.id)) return message.reply("They're already missing that role...");
   await(rMember.removeRole(gRole.id));
 
   try{
     await rMember.send(`RIP, you lost the ${gRole.name} role.`)
   }catch(e){
-    message.channel.send(`RIP to <@${rMember.id}>, We removed ${gRole.name} from them. We tried to DM them, but their DMs are locked.`)
+    message.channel.send(`RIP <@${rMember.id}>. They are no longer ${gRole.name}. We tried to keep the announcement private, but their DMs are locked.`)
   }
 }
 
