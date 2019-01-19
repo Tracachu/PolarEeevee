43fdf80f1aaaab470fdd70b1cd51e58f3a6f801f
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2019-01-19 15:31:16 -0500

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Hmph. I've cleaned ${args[0]} messages from your channel. Try to be more tidy next time, yeah?`).then(msg => msg.delete(2000));
+  message.channel.send(`Hmph. I've cleaned ${args[0]} messages from your channel. Try to be more tidy next time, yeah?`).then(msg => msg.delete(10000));
 });
 
 }
