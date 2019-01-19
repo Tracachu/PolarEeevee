a9e89d594482eaaca0df8ca298540ab75085149f
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2019-01-19 16:21:35 -0500

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
+  if(!args[0]) return message.channel.send("How about you tell me where the mess is so I can take care of it?");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Hmph. I've cleaned ${args[0]} messages from your channel. Try to be more tidy next time, yeah?`).then(msg => msg.delete(10000));
 });
