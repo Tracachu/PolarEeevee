48bca8f32c05356da33d5f6027578f08f26c1ee7
LeolaniLiaMay <44443373+LeolaniLiaMay@users.noreply.github.com>
2019-01-22 15:41:44 -0500

    Add files via upload

---
+++ commands/leave.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+let bot = new Discord.Client();
+
+exports.run = (bot, message, args, prefix) => {
+
+//Get the queue
+let server = bot.serverQueues[message.guild.id];
+
+//Check if anything is playing
+if(!server.queue[0]) return message.channel.send(`${bot.emojis.get("536599762195972117")} | Sorry! There is nothing playing.`);
+
+//Leave the voice channel
+message.guild.me.voiceChannel.leave();
+
+//Send the message
+message.channel.send(`${bot.emojis.get("536599762195972117")} | Leaving...`);
+
+}
