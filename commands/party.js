3ee2a3ba54fcf8a2205fbe9ebd47ac9805a5b6bb
TracaPoo <42890509+Tracachu@users.noreply.github.com>
2019-06-09 17:51:12 +0100

    Update party.js

--- commands/party.js
+++ commands/party.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setImage("https://i.imgur.com/a0uDLtt.gif")
     .setTitle(`Dance Party! 🎉`)
-    .setFooter(`Original gif by [_whitelate_](https://mobile.twitter.com/_whitelate_?lang=es) `)
+    .setFooter("Original gif by [_whitelate_](https://mobile.twitter.com/_whitelate_?lang=es)")
     .setColor(bot.embedColor)
     message.channel.send(embed);
 
